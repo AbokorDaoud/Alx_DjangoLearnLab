1004264a3cec146888b98cf114bd18b5b920df7e
Abokor Daoud <abokordaoud22@gmail.com>
2024-11-30 12:15:25 +0300

    commit for the task1 advanced api

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 from django_filters.rest_framework import DjangoFilterBackend  
 from .models import Book
 from .serializers import BookSerializer
-
+from django_filters import rest_framework
 # ListView: Retrieve all books with filtering, searching, and ordering
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
