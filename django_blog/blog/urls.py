4b3053c51eb0dd50998f4605d9b24efccf52e956
Abokor Daoud <abokordaoud22@gmail.com>
2024-12-02 19:52:43 +0300

    Add tags__name__icontains filter for search

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
-    
+    path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='posts_by_tag'),
     # Blog post URLs
     path('posts/', PostListView.as_view(), name='post-list'),
     path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
