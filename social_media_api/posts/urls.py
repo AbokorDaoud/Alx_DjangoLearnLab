d851898da4e94e0fab116995f197fa322db10ba5
Abokor Daoud <abokordaoud22@gmail.com>
2024-12-10 08:05:53 +0300

    commit for the social media

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,10 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
 
+app_name = 'posts'
+
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('feed/', PostViewSet.as_view({'get': 'feed'}), name='post-feed'),
 ]
