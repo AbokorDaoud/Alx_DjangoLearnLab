82fdb8aa6cd376bb67dbbafaa35dec259029063a
Abokor Daoud <abokordaoud22@gmail.com>
2024-12-10 08:28:06 +0300

    commit for the social media

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -152,10 +152,14 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
