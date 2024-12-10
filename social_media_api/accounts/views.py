c9347781999b6f9606591eeccfccf2e1fcf6f4eb
Abokor Daoud <abokordaoud22@gmail.com>
2024-12-10 08:00:24 +0300

    commit for the social media

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -10,20 +10,19 @@ from rest_framework.decorators import action
 
 User = get_user_model()
 
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    permission_classes = (permissions.AllowAny,)
+class RegisterView(generics.GenericAPIView):
     serializer_class = UserRegistrationSerializer
+    permission_classes = (permissions.AllowAny,)
 
-    def create(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         token, created = Token.objects.get_or_create(user=user)
         return Response({
-            "user": UserSerializer(user, context=self.get_serializer_context()).data,
-            "token": token.key
-        })
+            'user': UserSerializer(user, context=self.get_serializer_context()).data,
+            'token': token.key
+        }, status=status.HTTP_201_CREATED)
 
 class CustomAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
